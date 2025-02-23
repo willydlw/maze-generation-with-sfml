d819f2ef64eb8ca0cd3a33c7b47c53839aed7f55
willydlw <willydlw@comcast.net>
2025-02-23 10:50:35 -0700

    correctly drawing border walls and individual cell right and down walls

--- mazegen/src/maze.cpp
+++ mazegen/src/maze.cpp
@@ -32,57 +32,63 @@ namespace mazegen{
 
     void Maze::draw(sf::RenderWindow& window)
     {
-        sf::RectangleShape leftBorder(sf::Vector2f{static_cast<float>(mWallThickness), 
-                static_cast<float>(mWallLength * mRows)});
+        // left and right border shapes
+        sf::RectangleShape verticalBorder(sf::Vector2f{static_cast<float>(mWallThickness), 
+                static_cast<float>(mWallLength * mRows + 2 * mWallThickness)});
 
-        leftBorder.setFillColor(sf::Color{255, 255, 0});
+        verticalBorder.setFillColor(sf::Color{255, 255, 0});
 
-        leftBorder.setPosition(sf::Vector2f{0.0f, 0.0f});
-        window.draw(leftBorder);
+        // left border
+        verticalBorder.setPosition(sf::Vector2f{0.0f, 0.0f});
+        window.draw(verticalBorder);
 
-        sf::RectangleShape bottomBorder({static_cast<float>(mWallLength * mCols), 
+        // right border
+        verticalBorder.setPosition(sf::Vector2f{static_cast<float>(window.getSize().x - mWallThickness), 0.0f});
+        window.draw(verticalBorder);
+
+        // top and bottom border shape
+        sf::RectangleShape horizontalBorder({static_cast<float>(mWallLength * mCols + 2 * mWallThickness), 
                 static_cast<float>(mWallThickness)});
 
-        bottomBorder.setFillColor(sf::Color{0,0,255} );
-        bottomBorder.setPosition(sf::Vector2f{0.0f, 
+        horizontalBorder.setFillColor(sf::Color{0,0,255} );
+
+        // top border
+        horizontalBorder.setPosition(sf::Vector2f{0.0f, 0.0f});
+        window.draw(horizontalBorder);
+
+        // bottom border
+        horizontalBorder.setPosition(sf::Vector2f{0.0f, 
                 static_cast<float>(window.getSize().y - mWallThickness)});
-        window.draw(bottomBorder);
+        window.draw(horizontalBorder);
 
-        sf::RectangleShape vertWall(sf::Vector2f{static_cast<float>(mWallThickness), static_cast<float>(mCellSize)});
-        vertWall.setFillColor(CELLCOLORS::WALL);
+        sf::RectangleShape rightWall(sf::Vector2f{static_cast<float>(mWallThickness), static_cast<float>(mCellSize)});
+        rightWall.setFillColor(CELLCOLORS::WALL);
 
 
-        sf::RectangleShape horzWall(sf::Vector2f{static_cast<float>(mCellSize), static_cast<float>(mWallThickness)});
-        horzWall.setFillColor(CELLCOLORS::WALL);
+        sf::RectangleShape downWall(sf::Vector2f{static_cast<float>(mCellSize), static_cast<float>(mWallThickness)});
+        downWall.setFillColor(CELLCOLORS::WALL);
 
-        static int printCount = 0;
 
         for(int r = 0; r < mRows; r++){
             for(int c = 0; c < mCols; c++){
                 int i = r * mCols + c;
                 
                 if(mGrid[i].state & Cell::DOWN_WALL){
-                    //float x = static_cast<float>(c * mCellSize);
-                    //float y = static_cast<float>(r * mCols + mCellSize);
+                    float x = static_cast<float>(c * mCellSize + mWallThickness);
+                    float y = static_cast<float>((r+1) * mCellSize);
 
-                    //if(printCount == 0)
-                    //std::cerr << "horzWall, r: " << r << ", c: " << c
-                    //    << ", x: " << x << ", y: " << y << "\n";
-
-                    //horzWall.setPosition(sf::Vector2f{x, y});
-                    //window.draw(horzWall);
+                    downWall.setPosition(sf::Vector2f{x, y});
+                    window.draw(downWall);
                 }
 
                 if(mGrid[i].state & Cell::RIGHT_WALL){
                     float x = static_cast<float>( (c + 1) * mCellSize);
                     float y = static_cast<float>(r * mCellSize + mWallThickness);
-                    vertWall.setPosition(sf::Vector2f{x, y});
-                    window.draw(vertWall);
+                    rightWall.setPosition(sf::Vector2f{x, y});
+                    window.draw(rightWall);
                 }
             }
         }
-
-        printCount++;
     }
 
 } // end namespace mazegen
